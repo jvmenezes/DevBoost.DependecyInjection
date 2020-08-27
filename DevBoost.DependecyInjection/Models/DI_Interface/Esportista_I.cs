a8d8e35c0c1efab29745f2ac3faa97e473ae35ef
Jonathan Vieira Menezes <jonathanm@me.local>
2020-08-26 23:32:37 -0300

    Pequenas correções

--- DevBoost.DependecyInjection/Models/DI_Interface/Esportista_I.cs
+++ DevBoost.DependecyInjection/Models/DI_Interface/Esportista_I.cs
@@ -4,16 +4,21 @@ using System;
 namespace DevBoost.DependecyInjection.Models.DI_Interface
 {
     public class Esportista_I : IAtividadesDI_I
-    {        
+    {
+
+        private IAtividades _atividades;
 
         public string Nome { get; set; }
         public int Idade { get; set; }
         public string Pais { get; set; }
-        public string Atividade { get; set; }
+        public string Atividade
+        {
+            get { return _atividades.GetNome(); }
+        }
 
         public void setAtividade(IAtividades atividades)
         {
-            Atividade = atividades.GetNome();
+            _atividades = atividades;
         }
     }
 }
